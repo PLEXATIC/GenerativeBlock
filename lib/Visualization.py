de98631067b2d2da7a2c1cbe6589ffb821598edf
PLEXATIC <kim.mueller1999@gmail.com>
2023-09-05 12:22:20 +0200

    Extended Visualization lib to allow fullscreen and exit on esc

--- lib/Visualization.py
+++ lib/Visualization.py
@@ -2,23 +2,29 @@ import pygame as pg
 
 class Visualization:
     """Helper class to easily get visualizations by modifying the update method. For this, override the update method"""
-    def __init__(self, resolution=(640, 480), fps=60):
+    def __init__(self, resolution=(640, 480), fps=60, fullscreen=True):
         self.resolution = resolution
         self.fps = fps
         pg.init()
         self.clock = pg.time.Clock()
         self.running = True
-        self.screen = pg.display.set_mode(self.resolution)
+        if fullscreen:
+            self.screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
+            self.resolution = self.screen.get_size()
+        else:
+            self.screen = pg.display.set_mode(self.resolution)
 
     def update(self):
         pass
-
 
     def run(self):
         while self.running:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     self.running = False
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_ESCAPE:
+                        self.running = False
             self.update()
             pg.display.flip()
             self.clock.tick(self.fps)

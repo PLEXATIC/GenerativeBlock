56ebf724898fe739d000c0e7a1f503bdc54f531e
PLEXATIC <kim.mueller1999@gmail.com>
2023-09-08 10:32:14 +0200

    fixed wrong physics

--- Day04/CircleMachine.py
+++ Day04/CircleMachine.py
@@ -14,10 +14,10 @@ class BaseCircle:
                 child.update(t)
 
     def draw(self, screen):
-        pg.draw.circle(screen, (255, 255, 255), self.get_center_position().astype(int), self.radius, 1)
-        pg.draw.circle(screen, (255, 255, 255), self.get_swinger_position().astype(int), 3, 0)
+        pg.draw.circle(screen, (125, 125, 125), self.get_center_position().astype(int), self.radius, 1)
+        pg.draw.circle(screen, (255, 255, 255), self.get_swinger_position().astype(int), 3, 1)
         for child in self.children:
-            pg.draw.line(screen, (255, 255, 255), self.get_swinger_position().astype(int), child.get_swinger_position().astype(int), 1)
+            pg.draw.line(screen, (255, 255, 255), self.get_swinger_position().astype(int), child.get_center_position().astype(int), 1)
             child.draw(screen)
 
 class FixedCircle(BaseCircle):
@@ -49,21 +49,18 @@ class MovingCircle(BaseCircle):
             self.children = []
 
     def get_center_position(self):
-        parent_rot = 0
-        if self.parent:
-            parent_rot = self.parent.angle
-        return self.parent.get_swinger_position() + self.parent.radius * np.array([math.cos(self.parent.angle+parent_rot), math.sin(self.parent.angle+parent_rot)])
+        return self.parent.get_swinger_position()
     
     def get_swinger_position(self):
         parent_rot = 0
-        #if self.parent:
-        #    parent_rot = self.parent.angle
+        if self.parent:
+            parent_rot = self.parent.angle
         return self.get_center_position() + self.radius * np.array([math.cos(self.angle+parent_rot), math.sin(self.angle+parent_rot)])
 
 class CircleMachineVisualization(Visualization):
     def __init__(self):
         super().__init__()
-        baseCircle = FixedCircle(pos=np.array([self.resolution[0] / 2, self.resolution[1] / 2]), radius=150, speed=np.pi*0.1, angle=0)
+        baseCircle = FixedCircle(pos=np.array([self.resolution[0] / 2, self.resolution[1] / 2]), radius=60, speed=np.pi*2.8, angle=0)
         
         some_child = MovingCircle(parent=baseCircle, angle=0, radius=60, speed=np.pi*2.8)
         some_child.children.clear()
@@ -76,20 +73,25 @@ class CircleMachineVisualization(Visualization):
         yet_another_child = MovingCircle(parent=another_child, angle=0, radius=60, speed=np.pi*2.8)
         another_child.children.append(yet_another_child)
 
+
+        
+
+
         self.track = []
         self.fps = 60
-        self.debug = False
+        self.debug = True
 
     def update(self):
-        
         if pg.key.get_pressed()[pg.K_d]:
             self.debug = not self.debug
 
-        self.circles[0].update(1/self.fps)
+        self.circles[0].update(1/self.fps * 0.1)
         self.screen.fill((0, 0, 0))
+        
         for circle in self.circles:
             if self.debug:
                 circle.draw(self.screen)
+
         self.track.append(self.circles[0].children[0].children[0].children[0].get_swinger_position())
         if len(self.track) > 3:
             pg.draw.lines(self.screen, (255, 255, 255), False, self.track, 1)
